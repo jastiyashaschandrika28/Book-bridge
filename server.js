cf6c3d9f75518a9e895150f1318228c02fc6a242
jastiyashaschandrika28 <jastiyashas.cs24@bmsce.ac.in>
2025-12-30 20:24:18 +0530

    Update server.js

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use("/uploads", express.static("uploads")); // ✅ REQUIRED
 app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "public")));
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname,  "public","index.html"));
 });
@@ -142,6 +143,7 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () =>
   console.log(`Server running on port ${PORT}`)
 );
+
 
 
 
