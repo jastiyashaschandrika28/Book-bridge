0d63ba71ee4256b5835f213ad766ea05bed9136f
jastiyashaschandrika28 <jastiyashas.cs24@bmsce.ac.in>
2026-01-13 10:18:12 +0530

    Update server.js

--- server.js
+++ server.js
@@ -140,9 +140,16 @@ app.get("/uploads", async (req, res) => {
    SERVER START
 ======================= */
 const PORT = process.env.PORT || 5000;
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
 app.listen(PORT, () =>
   console.log(`Server running on port ${PORT}`)
 );
+
 
 
 
